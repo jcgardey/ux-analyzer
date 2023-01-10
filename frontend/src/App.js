843ce5084a7ddc90416b6fb4813c7461b154365f
juancruzgardey <juancruzgardey@hotmail.com>
2023-01-10 09:28:42 -0300

    add react router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,20 @@
 import { NavBar } from './components/NavBar';
 import { VersionsPage } from './pages/VersionsPage';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <VersionsPage />,
+    },
+  ]);
+
   return (
     <>
       <NavBar />
       <div className="w-4/5 mx-auto">
-        <VersionsPage />
+        <RouterProvider router={router} />
       </div>
     </>
   );
