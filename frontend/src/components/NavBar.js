f66ce3eb65f2c6836dec18676e2625042d02cf67
juancruzgardey <juancruzgardey@hotmail.com>
2023-01-24 14:19:52 -0300

    change navbar style

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -1,12 +1,14 @@
 export const NavBar = () => (
   <div
-    className={`w-full h-16 bg-sky-900 flex justify-between items-center px-2 py-0.25`}
+    className={`w-full h-16 bg-sky-900 flex justify-between items-center px-4 py-0.25`}
   >
     <div className="border-r border-white px-1.5">
-      <a className="text-xl text-white font-medium">UX-Analyzer</a>
+      <a className="text-2xl text-white font-semibold">UX-Analyzer</a>
     </div>
     <div>
-      <span className="text-white hover:">Nombre usuario</span>
+      <span className="text-white text-md font-medium">
+        Juan Cruz Gardey <i className="fa-solid fa-caret-down"></i>
+      </span>
     </div>
   </div>
 );
