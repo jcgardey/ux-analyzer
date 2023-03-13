fd83c66a2034991cfa245eb6b49f8063557e4d37
juancruzgardey <juancruzgardey@hotmail.com>
2023-03-13 14:51:25 -0300

    change NavBar styles

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -1,9 +1,14 @@
+import { Link } from 'react-router-dom';
+
 export const NavBar = () => (
   <div
     className={`w-full h-16 bg-sky-900 flex justify-between items-center px-4 py-0.25`}
   >
-    <div className="border-r border-white px-1.5">
-      <a className="text-2xl text-white font-semibold">UX-Analyzer</a>
+    <div>
+      <a className="text-2xl text-white font-semibold border-r border-white px-1.5">
+        UX-Analyzer
+      </a>
+      <a className="text-white text-lg font-medium mx-4">Evaluations</a>
     </div>
     <div>
       <span className="text-white text-md font-medium">
