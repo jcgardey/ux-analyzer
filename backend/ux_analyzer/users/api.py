d31f10f186b7f960dbda63394e959a118dfd82f5
Juan Cruz Gardey <juancruzgardey@hotmail.com>
2023-04-04 16:20:28 -0300

    add service to get logged user

--- backend/ux_analyzer/users/api.py
+++ backend/ux_analyzer/users/api.py
@@ -14,10 +14,15 @@ class LoginAPI(APIView):
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST) 
         user = authenticate(email=request.data['email'], password=request.data['password'])
         if user is None:
-            return Response({'error': 'Invalid Credentials'}, status=status.HTTP_401_UNAUTHORIZED)
+            return Response({'credentials': 'Invalid Credentials'}, status=status.HTTP_401_UNAUTHORIZED)
         login(request, user)
         _, token = AuthToken.objects.create(user=user)
-        return Response({ 'token': token })
+        return Response({ 'token': token, 'name': user.name })
+
+class GetLoggedUserAPI(APIView):
+
+    def get(self, request):
+        return Response({'name': request.user.name})
 
         
         
