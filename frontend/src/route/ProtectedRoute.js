1859ca432d25287d72fccf1c6919f7f945b5b988
Juan Cruz Gardey <juancruzgardey@hotmail.com>
2023-04-11 17:10:31 -0300

    add loading component

--- frontend/src/route/ProtectedRoute.js
+++ frontend/src/route/ProtectedRoute.js
@@ -1,11 +1,12 @@
 import { useContext } from 'react';
 import { Navigate } from 'react-router-dom';
+import { Loading } from '../components/Common/Loading';
 import { UserContext } from '../context/UserContext';
 
 export const ProtectedRoute = ({ children }) => {
   const { user, isAuthenticated } = useContext(UserContext);
   if (isAuthenticated && user === null) {
-    return <p>Cargando usuario..</p>;
+    return <Loading />;
   }
 
   if (!isAuthenticated) {
