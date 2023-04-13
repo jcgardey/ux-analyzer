206d34ce01f443ff4d4cfad8887e0d0226a2df42
juancruzgardey <juancruzgardey@hotmail.com>
2023-04-13 15:44:45 -0300

    add cotorro

--- backend/ux_analyzer/evaluations/api/evaluation_api.py
+++ backend/ux_analyzer/evaluations/api/evaluation_api.py
@@ -1,28 +1,40 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.permissions import IsAuthenticated
 from evaluations.serializers import EvaluationSerializer, FullEvaluationSerializer
 
 from evaluations.models import Evaluation
 
 class CreateEvaluationAPI(APIView):
+    permission_classes = [IsAuthenticated]
 
     def post(self, request):
-        evaluation = Evaluation.objects.create(evaluation_name=request.data['evaluation_name'])
+        evaluation = request.user.profile.evaluations.create(evaluation_name=request.data['evaluation_name'])
         return Response(EvaluationSerializer(evaluation).data, status=status.HTTP_201_CREATED)
 
 class DeleteEvaluationAPI(APIView):
     
     def delete(self, request, id):
-        return Response(Evaluation.objects.get(pk=id).delete())
+        evaluation = Evaluation.objects.get(pk=id)
+        if request.user.profile == evaluation.user:
+            return Response(evaluation.delete())
+        else:
+            return Response({'error': 'unauthorized'}, status=status.HTTP_401_UNAUTHORIZED)
 
 
 class ListEvaluationsApi(APIView):
+    permission_classes = [IsAuthenticated]
 
     def get(self, request):
-        return Response(EvaluationSerializer(Evaluation.objects.all(), many=True).data)
+        return Response(EvaluationSerializer(request.user.profile.evaluations.all(), many=True).data)
 
 class GetEvaluationAPI(APIView):
+    permission_classes = [IsAuthenticated]
 
     def get(self, request, id):
-        return Response(FullEvaluationSerializer(Evaluation.objects.get(pk=id)).data)
+        evaluation = Evaluation.objects.get(pk=id)
+        if request.user.profile == evaluation.user:
+            return Response(FullEvaluationSerializer(evaluation).data)
+        else:
+            return Response({'error': 'unauthorized'}, status=status.HTTP_401_UNAUTHORIZED)
