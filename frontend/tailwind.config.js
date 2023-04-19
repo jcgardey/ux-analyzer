1e33366c5e00f08e4b43e98215559ec03f38aedf
juancruzgardey <juancruzgardey@hotmail.com>
2023-04-19 11:53:01 -0300

    set custom colors and default fonts

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,17 +1,24 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
-    colors: {
-      yellow: '#FFE15D',
-      orange: '#F49D1A',
-      red: '#DC3535',
-      grey: '#f5f5f5',
+    fontFamily: {
+      sans: ['Fira Sans', ...defaultTheme.fontFamily.sans],
+      serif: [...defaultTheme.fontFamily.serif],
+      mono: [...defaultTheme.fontFamily.mono],
+    },
+    extend: {
+      colors: {
+        yellow: '#FFE15D',
+        orange: '#F49D1A',
+        red: '#DC3535',
+        grey: '#f5f5f5',
+      },
     },
   },
-  fontFamily: {
-    sans: ['Fira Sans', 'Arial', 'sans-serif'],
-  },
+
   plugins: [],
 };
+
